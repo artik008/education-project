2b8099503b27bd507abca2ef9bca32ba2c0bfeda
Artyom <artik008@yandex.ru>
2017-04-05 22:21:21 +0300

    fix

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,11 @@ class ProjectsController < ApplicationController
   def index
     @project = Project.all
   end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to "/"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'todos#index'
   post '/', to: 'todos#create'
 
-  resources :projects, only: [:show]
+  resources :projects, only: [:destroy]
   resources :todos, only: [:show, :create, :update, :destroy]
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
